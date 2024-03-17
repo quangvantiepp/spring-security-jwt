4fe5216f30d093935575bf2eca3936cb639a958a
minhtiep <minhtiepaa1@gmail.com>
2024-03-17 16:40:23 +0700

    start project

---
+++ src/main/java/nature/sales_website/SalesWebsiteApplication.java
@@ -0,0 +1,13 @@
+package nature.sales_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nature/sales_website/SalesWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package nature.sales_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
